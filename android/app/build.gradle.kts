a9fe9529688a8007699217f03c102b228320f3da
Ffhkkgfillugoopoohjjuuubh <marinaanm16@gmail.com>
2026-04-28 09:25:51 +0530

    Update NDK version and add core library desugaring
    Updated NDK version and added core library desugaring dependency.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,11 +7,12 @@ plugins {
 android {
     namespace = "com.example.ai_tutor"
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -32,6 +33,10 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
 
 flutter {
